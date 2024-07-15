5865332676d7b9d66c11110f3629c0a755d7f6a3
Fluffy <flaviofelderer@icloud.com>
2024-07-15 21:27:21 +0200

    changed to tasks and also moved everything to env files expect the token.json this needs to be in the /app directory when starting the bot

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,13 @@
 FROM python:3.12.4-slim
 
 # Set the working directory in the container
-WORKDIR /usr/src/app
+WORKDIR /app
 
 # Copy the current directory contents into the container at /usr/src/app
-COPY . .
+COPY . /app
 
 # Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Run app.py when the container launches
-CMD ["python", "./calendarbot.py"]
+CMD ["python", "./main.py"]
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
 import discord
+from discord.ext import tasks
 import os
-import asyncio
-import time
 from datetime import datetime, timezone
 from dateutil import parser
 import pytz
@@ -12,8 +10,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from dotenv import load_dotenv
 import logging
-from fastapi import FastAPI
-import uvicorn
+import json
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -24,23 +21,20 @@ sent_message = False
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-app = FastAPI()
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = discord.Client(intents=intents)
+client = discord.Client(intents=intents)
 
-TOKEN = os.getenv("TOKEN")
+google_calendar_token = os.getenv('GOOGLE_CALENDAR_TOKEN')
 
 
-@app.on_event("startup")
-async def startup_event():
-    logging.info("Starting up bot")
-    asyncio.create_task(bot.start(TOKEN))
-    await asyncio.sleep(5)
-    print(f"{bot.user} has connected to Discord!")
-    bot.loop.create_task(scheduled_upcoming_events())
+@client.event
+async def on_ready():
+    logging.info(f'{client.user} has connected to Discord!')
+    print(f'{client.user} has connected to Discord!')
+    sent_message_resetter.start()
+    upcoming_events.start()
 
 
 def google_calendar_events():
@@ -51,9 +45,7 @@ def google_calendar_events():
         - 'start': The start time of the event (in RFC 3339 format).
         - 'summary': The title of the event.
     """
-    creds = None
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -63,7 +55,7 @@ def google_calendar_events():
         service = build("calendar", "v3", credentials=creds)
         now = datetime.now(timezone.utc).isoformat()
         events_result = service.events().list(
-            calendarId="d4528073bf6b10d751df59727be5733d1f31beea44b0f03e1a60087e46dd7a19@group.calendar.google.com",
+            calendarId=os.getenv("GOOGLE_CALENDAR_ID"),
             timeMin=now,
             maxResults=10,
             singleEvents=True,
@@ -82,6 +74,7 @@ def google_calendar_events():
         return []
 
 
+@tasks.loop(minutes=5)
 async def upcoming_events():
     global sent_message
     try:
@@ -102,7 +95,7 @@ async def upcoming_events():
                 summary = event["summary"]
                 event_message += f"<t:{unix_timestamp}:R> - {summary}\n"
         if event_message and not sent_message:
-            channel = bot.get_channel(1253856173929529426)
+            channel = client.get_channel(os.getenv("CHANNEL_ID"))
             await channel.send(f"Upcoming events @everyone:\n{event_message}")
             sent_message = True
             logging.info(event_message)
@@ -112,44 +105,11 @@ async def upcoming_events():
         logging.error(f"An error occurred: {e}")
 
 
-def sent_message_resetter():
+@tasks.loop(minutes=30)
+async def sent_message_resetter():
     global sent_message
     sent_message = False
     logging.info("Sent message reset")
 
 
-async def scheduled_upcoming_events():
-    while True:
-        await upcoming_events()
-        await asyncio.sleep(1800)
-
-
-def scheduled_sent_message_resetter():
-    while True:
-        sent_message_resetter()
-        time.sleep(25200)
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/events/get")
-async def google_events_get():
-    event_list = google_calendar_events()
-    return event_list
-
-@app.get("/bot/sendmessage/{message}")
-async def send_message(message: str):
-    channel = bot.get_channel(1253856173929529426)
-    await channel.send(message)
-    return {"message": "Message sent!"}
-
-
-
-thread = threading.Thread(target=scheduled_sent_message_resetter)
-thread.start()
-
-if __name__ == '__main__':
-    uvicorn.run(app, host="localhost", port=5000)
+client.run(os.getenv("DISCORD_TOKEN"))
